f8d11d547fc961111d5a99f2c5435d4a36b7e783
kelooy <karol.osowski.97@gmail.com>
2019-11-02 13:12:10 +0100

    New logo and small action GUI corrects

--- src/pl/kelooystore/roadcalculator/ActionGUI.java
+++ src/pl/kelooystore/roadcalculator/ActionGUI.java
@@ -7,6 +7,19 @@ public class ActionGUI extends FrameTemplate
     private JPanel mainActionPanel;
     private JButton calculateButton;
     private JButton exitButton;
+    private JButton openConverterButton;
+    private JPanel buttonPanel;
+    private JCheckBox bothSidesCheckBox;
+    private JCheckBox considerWeightCheckBox;
+    private JTextArea outputArea;
+    private JPanel checkBoxPanel;
+    private JButton clearButton;
+    private JPanel inputPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
     private final String windowTitle = "Action Window - RoadCalculator";
 
     public ActionGUI()
